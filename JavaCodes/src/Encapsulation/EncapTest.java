50d35acff80706e65fdedf0982449a71dfa308e3
Saiyem Raiyan <64364859+SaiyemRaiyan@users.noreply.github.com>
2023-07-14 16:49:45 +0600

    Add files via upload

---
+++ JavaCodes/src/Encapsulation/EncapTest.java
@@ -0,0 +1,18 @@
+
+package Encapsulation;
+
+public class EncapTest
+{
+    public static void main(String[] args) 
+    {
+        
+        Person p1 = new Person();
+        
+        p1.setName("Raiyan");
+        p1.setAge(21);
+        System.out.println(p1.getName());
+        System.out.println(p1.getAge());
+        
+    }
+    
+}
